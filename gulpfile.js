969119366c3635aa20c3683ffcc28a7651b4e01c
dan goldman <bloodclaw69@gmail.com>
2016-01-01 23:40:20 -0500

    nightly build

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,46 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var concat = require('gulp-concat');
+var headerfooter = require('gulp-headerfooter');
+
+var jshint = require('gulp-jshint');
+    
+gulp.task('jshint', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('concat', function(){
+    gulp.src([
+        'src/**/*module.js',
+        'src/**/*.js'])
+        .pipe(headerfooter(
+            '(function() {\n    \'use strict\';\n\n',
+            '\n\n})();'))
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('dist'));
+});
  
+ 
+ gulp.task('copy', function(){
+     gulp.src(['src/index.html'])
+        .pipe(gulp.dest('dist'));
+        
+ });
 gulp.task('webserver', function() {
-  gulp.src('app')
+  gulp.src('dist')
     .pipe(webserver({
       livereload: true,
       directoryListing: true,
-      open: true
+ //s     open: true,
+      path : "/dist",
+      fallback: "index.html"
     }));
 });
+gulp.task('watchSrc', function(){
+    gulp.watch('src/**/*', ['jshint','copy', 'concat']);
+});
 
-gulp.task('default', ['webserver']);
+
+gulp.task('default', ['watchSrc', 'webserver']);
