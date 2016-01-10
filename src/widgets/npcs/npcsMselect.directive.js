760ea60525f0abaeb9eb64b99270ea793fc7ac44
grizmul <this_dan@yahoo.com>
2016-01-10 13:17:28 -0500

    sdf

--- src/widgets/npcs/npcsMselect.directive.js
+++ src/widgets/npcs/npcsMselect.directive.js
@@ -3,7 +3,7 @@ angular
     .directive('npcsMselect', Constructor);
 
 
-function Constructor(){
+function Constructor() {
     var directive = {
         bindToController: true,
         controller: Controller,
@@ -11,48 +11,62 @@ function Constructor(){
         restrict: 'A',
         templateUrl: 'npcsMselect.partial.html',
         link: link
-        
+
     };
-    
+
     return directive;
-    function link( scope, element, attrs){
+    function link(scope, element, attrs) {
+        //       scope.vm.e = element;
         //var select = element.find('select.npcsMselect');
         //var select = element.children()[1];
         
         //$( select).css(
-          //  {
-                //'width': width
-            //});        
+        //  {
+        //'width': width
+        //});       
+        var list = attrs['ng-options'];
+        scope.$watch(list, function () {
+            element.trigger('chosen:updated');console.log("watch called"); 
+        }, true);
+
+        scope.$watch(attrs['ngModel'], function() {
+            element.trigger('chosen:updated');console.log("watch called"); 
+        }, true);
+ 
     }
-   
+
 }
-Controller.$inject = ['es', '$timeout'];
-function Controller(es, $timeout){
+Controller.$inject = ['es', '$timeout', '$element', '$scope'];
+function Controller(es, $timeout, $element, $scope) {
     var vm = this;
     vm.npcs = ['A very long monster name goes here', 'Wolf', 'Orc', 'Goblin', 'Gnome', 'Elf', 'Vagabond'];
-    vm.rooms=[];
+    vm.rooms = [];
     vm.selected = [];
-    
-    
-    
-     es.search({
-            index: 'rooms',
-            q: 'name:*',
-            size: 10,
-            from: 0
-        }).then(function (resp) {
-                if (resp.hits.total > 0) {
-                console.log(resp.hits.hits);
-                for(var i=0; i<resp.hits.total; i ++)
-                    vm.rooms.push(resp.hits.hits[i]['_source']);
-            }
-            else {
-                console.log(resp.hits);
-            }            
+  
+   
+    es.search({
+        index: 'rooms',
+        q: 'name:*',
+        size: 10,
+        from: 0
+    }).then(function (resp) {
+
+        if (resp.hits.total > 0) {
+            console.log(resp.hits.hits);
+            for (var i = 0; i < resp.hits.total; i++)
+                vm.rooms.push(resp.hits.hits[i]['_source']); 
+       
+         //$element.find('.chosen').chosen();
+            //$element.find('chosen').trigger('change');   
             
-        }, function (err) {
-            console.log(err);
-        });
- 
-    
+        }
+        else {
+            console.log(resp.hits);
+        }
+
+    }, function (err) {
+        console.log(err);
+    });
+
+
 }
